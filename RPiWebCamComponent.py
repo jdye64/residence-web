8963ab22ba6b8ecf4e2e751d59848a33c550c3b7
Jeremy Dyer <jdye64@gmail.com>
2014-10-07 14:38:39 -0400

    Updates

--- RPiWebCamComponent.py
+++ RPiWebCamComponent.py
@@ -1,34 +1,30 @@
 # Component for controlling a WebCam connected to a RPi device
 from twisted.internet.defer import inlineCallbacks
 from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner
-from gpio import RPi
+from autobahn import wamp
 import base64
+import os
 
 class RPiWebCamComponent(ApplicationSession):
 
     @inlineCallbacks
     def onJoin(self, details):
-        self.deviceRegUID = yield self.call('com.jeremydyer.residence.rpi.join.request')
-        print "Device Registry Unique Identifier -> " + str(self.deviceRegUID)
+        print "RPiWebCamComponent is online!"
+        yield self.register(self)
+        yield self.subscribe(self)
 
-        #Registers the RPC components for this device with their unique values received from the device registry
-        self.rpi = RPi(self.deviceRegUID)
-        self.rpi.turnOnOutletRPC = "com.jeremydyer.gpio.power.{}.turnon".format(str(self.deviceRegUID))
-        self.rpi.turnOffOutletRPC = "com.jeremydyer.gpio.power.{}.turnoff".format(str(self.deviceRegUID))
-        self.rpi.updateDeviceRPC = "com.jeremydyer.gpio.residence.{}.update".format(str(self.deviceRegUID))
-        yield self.register(self.turn_on_outlet, self.rpi.turnOnOutletRPC)
-        yield self.register(self.turn_off_outlet, self.rpi.turnOffOutletRPC)
-        yield self.register(self.update_device, self.rpi.updateDeviceRPC)
-
-        #Now that all of the registration has occured call back and give the list of subscriptions you have
-        self.deviceRegUID = yield self.call('com.jeremydyer.residence.rpi.join', self.rpi.to_json())
-
-        #Publish a message letting all connected devices know that a newly connected device is now available
-        yield self.publish('com.jeremydyer.residence.rpi.join.notify', self.rpi.to_json())
-
+    @wamp.register(u'com.jeremydyer.residence.rpi.webcam.takesnapshot')
     def take_snapshot(self):
-        fd = open("yourfile.ext", "rb")
+
+        print "Taking snapshot image"
+        # os.makedirs('/home/pi/images')
+        # cmd = "fswebcam -r 400x400 /home/pi/images/image.jpg"
+        # os.system(cmd)
+        #
+        # fd = open("/home/pi/images/image.jpg", "rb")
+        fd = open("/home/pi/moose.jpg")
         encoded_string = base64.b64encode(fd.read())
+        return {"image": encoded_string}
 
 
 if __name__ == '__main__':
--- TestSave.py
+++
@@ -1,6 +0,0 @@
-__author__ = 'jeremydyer'
-
-from gpio import RPi
-
-rpi = RPi(1)
-rpi.save()
