2876772a2404b7820715c3b810ae9f3a9bbc8772
Jeremy Dyer <jdye64@gmail.com>
2014-10-08 17:12:45 -0400

    Added a very basic cache mechanism to prevent long download times from S3

--- RPiAudioPlaybackComponent.py
+++ RPiAudioPlaybackComponent.py
@@ -2,10 +2,48 @@
 from twisted.internet.defer import inlineCallbacks
 from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner
 from autobahn import wamp
-import time
 import os
 
+#Caches audio files downloaded from the S3 bucket to the local device for faster load times
+class LocalAudioCacheStore:
+
+    s3url = None
+    cache_dir = None  #Cache directory on the local device
+    cachedFilesMap = {}    #Map of the cached URLs and their corelating file on the local filesystem.
+
+    def __init__(self, cache_dir, s3Url):
+        self.s3url = s3Url
+        self.cache_dir = cache_dir
+        print "Creating Audio Cache Store instance"
+        print "Audio Cached from " + str(s3Url)
+
+    def play_cached_file(self, url):
+        cache_file = self.cachedFilesMap.get(url)
+        if cache_file == None:
+            cache_file = self.cache_audio(url)
+            self.cachedFilesMap[url] = cache_file
+
+        # Plays the cached file
+        #os.system('mpg123 ' + destfile + ' &')
+        os.system('aplay ' + cache_file)
+
+    def cache_audio(self, url):
+        print "Downloading and caching the audio file " + str(url)
+        url_parts = url.split("/")
+        filename = url_parts[len(url_parts) - 1]
+        print str(filename)
+        destfile = self.cache_dir + filename
+
+        downloadcmd = "wget " + url + " -O " + destfile
+        print "Running download cmd " + downloadcmd
+        os.system(downloadcmd)
+        print "S3 download is complete"
+        return destfile
+
+
 class RPiAudioPlaybackComponent(ApplicationSession):
+
+    cache = LocalAudioCacheStore("/home/pi/.audio/", "https://s3.amazonaws.com/makeandbuild/courier/audio/")
 
     @inlineCallbacks
     def onJoin(self, details):
@@ -19,22 +57,15 @@ class RPiAudioPlaybackComponent(ApplicationSession):
 
         # Source file. This will ultimately be present in the JSON payload received by this method.
         sourceURL = "https://s3.amazonaws.com/makeandbuild/courier/audio/1.wav"
-
-        print "Source URL " + sourceURL
-        timestamp = time.time()
-        print "Timestamp " + str(timestamp)
-        destfile = '/home/pi/.audio/' + str(timestamp) + ".wav"
-
-        downloadcmd = "wget " + sourceURL + " -O " + destfile
-        print "Running download cmd " + downloadcmd
-        os.system(downloadcmd)
-        print "S3 download is complete"
-
-        #os.system('mpg123 ' + destfile + ' &')
-        os.system('aplay ' + destfile)
-        os.remove(destfile)
-        print "Removed cache S3 file"
+        self.cache.play_cached_file(sourceURL)
 
 if __name__ == '__main__':
-    runner = ApplicationRunner("ws://pi.jeremydyer.me:9000/ws", "realm1")
-    runner.run(RPiAudioPlaybackComponent)
+    #runner = ApplicationRunner("ws://pi.jeremydyer.me:9000/ws", "realm1")
+    #runner.run(RPiAudioPlaybackComponent)
+
+    test = RPiAudioPlaybackComponent()
+    test.cache.play_cached_file("https://s3.amazonaws.com/makeandbuild/courier/audio/1.wav")
+    test.cache.play_cached_file("https://s3.amazonaws.com/makeandbuild/courier/audio/1.wav")
+    test.cache.play_cached_file("https://s3.amazonaws.com/makeandbuild/courier/audio/2.wav")
+    test.cache.play_cached_file("https://s3.amazonaws.com/makeandbuild/courier/audio/3.wav")
+    test.cache.play_cached_file("https://s3.amazonaws.com/makeandbuild/courier/audio/2.wav")
